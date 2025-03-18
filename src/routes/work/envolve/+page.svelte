10b53aca677d0e81382fccf985081a4421884221
Lisa <hello@lisasundberg.com>
2025-03-18 14:14:05 +0100

    Add Envolve page

---
+++ src/routes/work/envolve/+page.svelte
@@ -0,0 +1,68 @@
+<article>
+	<div class="text">
+		<h1>Envolve</h1>
+		<div class="info">
+			<p class="label">Project at Another State 2019</p>
+			<p class="label">My responsibility: co-ideation, all development</p>
+			<p class="label">Technologies: React, GSAP, Sass</p>
+		</div>
+		<div class="body">
+			<!-- <p class="p-small">Web presence for Stockholm based brand experience agency Homage.</p>
+			<p class="p-small">
+				On this short and intense project I collaborated closely with design from start to end. We
+				worked in total around two weeks to create an unconventional website with the purpose to
+				showcase work and attract new clients as well as staff.
+			</p>
+
+			<p class="p-small">
+				Was awarded an <a
+					href="https://www.awwwards.com/sites/homage-2"
+					target="_blank"
+					rel="noopener">Awwwards Honorable Mention</a
+				>.
+			</p> -->
+		</div>
+	</div>
+
+	<div class="images">
+		<enhanced:img class="image" src="./assets/envolve-1.jpg" alt="Envolve" />
+	</div>
+</article>
+
+<style>
+	article {
+		display: flex;
+		flex-direction: row-reverse;
+		align-items: flex-start;
+		gap: var(--content-margin);
+	}
+
+	h1 {
+		font-family: var(--font-display);
+	}
+
+	.info {
+		margin-top: 1em;
+	}
+
+	.text {
+		position: sticky;
+		top: 6em;
+	}
+
+	.images {
+		width: 100%;
+		display: flex;
+		flex-direction: column;
+		gap: 1rem;
+	}
+
+	.image {
+		width: 100%;
+		height: auto;
+	}
+
+	.body {
+		margin-top: 2em;
+	}
+</style>
