b0ccadff4b8f914755715f487df47af56613e166
Lisa <hello@lisasundberg.com>
2025-04-24 15:39:39 +0200

    Clean up page transition

--- src/lib/components/PageTransition.svelte
+++ src/lib/components/PageTransition.svelte
@@ -1,21 +1,29 @@
 <script lang="ts">
-	import { fly, fade } from 'svelte/transition';
+	import { fade } from 'svelte/transition';
 
 	interface Props {
 		url: string;
 		children?: import('svelte').Snippet;
 	}
-	let { url, children } = $props();
+	let { url, children }: Props = $props();
 
-	const pageTransitionDuration = 250;
+	const duration = 200;
 </script>
 
 {#key url}
-	<main
+	<div
 		class="page-transition"
-		in:fade={{ duration: pageTransitionDuration, delay: pageTransitionDuration }}
-		out:fade={{ duration: pageTransitionDuration }}
+		in:fade={{ duration: duration, delay: duration }}
+		out:fade={{ duration }}
 	>
 		{@render children?.()}
-	</main>
+	</div>
 {/key}
+
+<style>
+	.page-transition {
+		grid-column: full;
+		display: grid;
+		grid-template-columns: subgrid;
+	}
+</style>
