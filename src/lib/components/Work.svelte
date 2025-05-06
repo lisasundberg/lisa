ac416f9e5a7e5bb20730d8c684b08b6e6b208ecf
Lisa <hello@lisasundberg.com>
2025-05-06 06:08:59 +0200

    Disable animate import

--- src/lib/components/Work.svelte
+++ src/lib/components/Work.svelte
@@ -2,7 +2,7 @@
 	import { onMount } from 'svelte';
 	import { gsap } from 'gsap';
 
-	import { animate } from '$lib/actions/animate';
+	// import { animate } from '$lib/actions/animate';
 	// import { split } from '$lib/actions/textSplitter';
 
 	import WorkItem from '$lib/components/WorkItem.svelte';
