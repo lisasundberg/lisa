6d0450a3d510e2512ca5c91f024f04ae11c56a38
Lisa <hello@lisasundberg.com>
2025-05-15 16:05:12 +0200

    Redo+clean up work section reveal with pure GSAP, no Svelte action
    Because it was overly complicated

--- src/lib/components/Work.svelte
+++ src/lib/components/Work.svelte
@@ -3,13 +3,15 @@
 	import { SplitText } from 'gsap/SplitText';
 	import { ScrollTrigger } from 'gsap/ScrollTrigger';
 
+	import { browser } from '$app/environment';
+	import { INVERTED_CLASSNAME } from '$lib/stores/theme';
+
 	import Button from '$lib/components/Button.svelte';
 
 	import Homage from '$lib/assets/homage/homage-mockup-1.jpg?enhanced';
 	import AH from '$lib/assets/akademiskahus/ah-mockup-1.jpg?enhanced';
 	import Envolve from '$lib/assets/envolve/envolve-cover.jpg?enhanced';
-	import { onMount } from 'svelte';
-	// import { onMount } from 'svelte';
+	import { onMount, onDestroy } from 'svelte';
 
 	const images = [
 		{
@@ -26,47 +28,14 @@
 		}
 	];
 
-	// let currentImageIndex: number | null = $state(null);
-	// function handleMouseEnter(i: number) {
-	// 	currentImageIndex = i;
-
-	// 	gsap.to(`[data-work-image="${i}]"`, { opacity: 1 });
-
-	// 	console.log('in', currentImageIndex);
-	// }
-
-	// function handleMouseLeave(i: number) {
-	// 	currentImageIndex = null;
-
-	// 	gsap.to(`[data-work-image="${i}"]`, { opacity: 0 });
-	// 	console.log('out');
-	// }
-
-	// onMount(() => {
-	// 	const links = document.querySelectorAll('[data-work-item]');
-
-	// 	links.forEach((link, i) => {
-	// 		link.addEventListener('mouseenter', () => handleMouseEnter(i));
-	// 		link.addEventListener('mouseleave', () => handleMouseLeave(i));
-	// 	});
-	// });
-
 	let workSection: HTMLElement;
 	let heading: HTMLElement;
 	let splitHeading: SplitText;
-	let timeline: gsap.core.Timeline;
+	let textTimeline: gsap.core.Timeline;
+	let bgTimeline: gsap.core.Timeline;
 
-	onMount(() => {
-		gsap.registerPlugin(SplitText);
-		gsap.registerPlugin(ScrollTrigger);
-
-		const splitParams = {
-			type: 'chars',
-			charsClass: 'work-char',
-			smartWrap: true
-		};
-
-		timeline = gsap.timeline({
+	function text() {
+		textTimeline = gsap.timeline({
 			scrollTrigger: {
 				trigger: workSection,
 				start: 'top -=5%',
@@ -77,57 +46,117 @@
 				markers: true
 			},
 			onComplete: () => {
-				splitHeading.revert();
+				// splitHeading.revert(); // Causes jump
+
+				if (textTimeline.scrollTrigger) {
+					textTimeline.scrollTrigger.kill();
+					textTimeline.kill();
+				}
 			}
 		});
 
-		document.fonts.ready.then(() => {
-			splitHeading = SplitText.create(heading, splitParams);
+		const splitParams = {
+			type: 'chars, lines',
+			smartWrap: true,
+			mask: 'lines' as 'lines'
+		};
+		splitHeading = SplitText.create(heading, splitParams);
 
-			gsap.set('[data-work-item]', {
-				pointerEvents: 'none'
-			});
+		gsap.set('[data-work-item]', {
+			pointerEvents: 'none'
+		});
 
-			timeline
-				.from(splitHeading.chars, {
-					filter: 'blur(10px)',
-					opacity: 0,
-					willChange: 'filter, opacity',
-					duration: 0.5,
-					stagger: 0.02
-				})
-				.from('[data-work-image]', {
-					opacity: 0,
-					stagger: 4,
-					duration: 1,
-					delay: 1
-				})
-				.to('[data-work-image]', {
-					opacity: 0,
-					duration: 1,
-					delay: 2
-				})
-				.from('[data-work-body]', {
+		textTimeline
+			// .from(splitHeading.chars, {
+			// 	filter: 'blur(10px)',
+			// 	opacity: 0,
+			// 	willChange: 'filter, opacity',
+			// 	stagger: {
+			// 		amount: 2
+			// 	}
+			// })
+			.from(splitHeading.chars, {
+				yPercent: 100,
+				autoAlpha: 0,
+				stagger: 0.02,
+				duration: 0.5
+			})
+			.from('[data-work-image]', {
+				opacity: 0,
+				stagger: 4,
+				duration: 1,
+				delay: 1
+			})
+			.to('[data-work-images]', {
+				opacity: 0,
+				duration: 1,
+				delay: 2
+			})
+			.from('[data-work-body]', {
+				opacity: 0,
+				yPercent: 50,
+				duration: 2,
+				ease: 'power4.out'
+			})
+			.set('[data-work-item', {
+				pointerEvents: 'auto'
+			})
+			.from(
+				'[data-work-button]',
+				{
 					opacity: 0,
 					yPercent: 50,
 					duration: 2,
 					ease: 'power4.out'
-				})
-				.from(
-					'[data-work-button]',
-					{
-						opacity: 0,
-						yPercent: 50,
-						duration: 2,
-						ease: 'power4.out'
-					},
-					'-=1'
-				)
-				.from('[data-work-body]', {
-					display: 'block',
-					duration: 5
-				});
+				},
+				'-=1'
+			)
+			.from('[data-work-body]', {
+				display: 'block',
+				duration: 5
+			});
+
+		return textTimeline;
+	}
+
+	function bg() {
+		bgTimeline = gsap.timeline({
+			scrollTrigger: {
+				trigger: workSection,
+				start: 'top -=5%',
+				end: '+=105%',
+				scrub: 4,
+				onEnter: () => (browser ? document.body.classList.add(INVERTED_CLASSNAME) : null), // Add class when entering the trigger
+				onLeaveBack: () => (browser ? document.body.classList.remove(INVERTED_CLASSNAME) : null) // Remove class when scrolling back
+			},
+			onComplete: () => {
+				if (bgTimeline.scrollTrigger) {
+					bgTimeline.scrollTrigger.kill();
+					bgTimeline.kill();
+				}
+			}
 		});
+
+		bgTimeline.from('body', {
+			onStart: () => (browser ? document.body.classList.add(INVERTED_CLASSNAME) : null),
+			onReverseComplete: () => (browser ? document.body.classList.remove(INVERTED_CLASSNAME) : null)
+		});
+
+		return bgTimeline;
+	}
+
+	onMount(() => {
+		gsap.registerPlugin(SplitText);
+		gsap.registerPlugin(ScrollTrigger);
+
+		document.fonts.ready.then(() => {
+			text();
+			bg();
+		});
+	});
+
+	onDestroy(() => {
+		ScrollTrigger.getAll().forEach((trigger) => trigger.kill());
 	});
 </script>
 
@@ -138,14 +167,6 @@
 			<a class="work-item" href="/work/akademiskahus" data-work-item="0">Akademiska Hus,</a>
 			<a class="work-item" href="/work/homage" data-work-item="1">Homage</a> and
 			<a class="work-item" href="/work/envolve" data-work-item="2">Envolve. </a>
-			<!-- <WorkItem
-				title="Akademiska Hus,"
-				images={[AH]}
-				link="/work/akademiskahus"
-				index={0}
-			/>
-			<WorkItem title="Homage" images={[Homage]} link="/work/homage" index={1} /> and
-			<WorkItem title="Envolve." images={[Envolve]} link="/work/envolve" index={2} /> -->
 		</p>
 
 		<p class="body p-xsmall" data-work-body>
@@ -220,7 +241,6 @@
 		text-transform: none;
 		font-size: 1.25em;
 		letter-spacing: 0;
-		transition: color 0.2s linear;
 		line-height: 0.8;
 
 		&::after {
