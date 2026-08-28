9d9714c601bff76138c235be618cf839f59380b0
Lisa <hello@lisasundberg.com>
2026-08-28 11:25:31 +0200

    Add new intro WIP

---
+++ src/lib/components/Hero.svelte
@@ -0,0 +1,96 @@
+<script lang="ts">
+	import { onDestroy, onMount } from 'svelte';
+	import { gsap } from 'gsap';
+
+	let intro: HTMLElement;
+	let context: gsap.Context;
+
+	onMount(() => {
+		document.fonts.ready.then(() => {
+			context = gsap.context(() => {
+				const text = gsap.utils.toArray<HTMLElement>('.text');
+
+				gsap.fromTo(
+					text,
+					{
+						yPercent: 100,
+						autoAlpha: 0
+					},
+					{
+						yPercent: 0,
+						autoAlpha: 1,
+						duration: 0.7,
+						stagger: 0.1,
+						ease: 'power4.out'
+					}
+				);
+			}, intro);
+		});
+	});
+
+	onDestroy(() => context?.revert());
+</script>
+
+<section class="intro" bind:this={intro}>
+	<div class="row">
+		<div class="mask -hello"><p class="text preamble">Hello, my name is</p></div>
+		<div class="mask -name"><h1 class="text heading">Lisa Sundberg</h1></div>
+		<div class="mask -intro"><p class="text preamble">and I'm a</p></div>
+	</div>
+
+	<div class="row">
+		<div class="mask -title"><h2 class="text heading">design engineer</h2></div>
+	</div>
+</section>
+
+<style>
+	.intro {
+		margin-block: 25dvh;
+	}
+
+	.row {
+		display: flex;
+		flex-wrap: wrap;
+		gap: 0.8em;
+	}
+
+	.text {
+		opacity: 0;
+		margin-block: 0;
+		text-box-trim: trim-both;
+	}
+
+	.mask {
+		display: inline-block;
+		overflow: hidden;
+
+		&.-hello {
+			margin-top: 0.2em;
+		}
+
+		&.-intro {
+			margin-top: auto;
+			margin-bottom: 0.35em;
+		}
+
+		&.-title {
+			margin-top: -0.375em;
+		}
+	}
+
+	.preamble {
+		font-family: var(--font-display);
+		font-size: var(--font-size-body-small);
+		margin: 0;
+	}
+
+	.heading {
+		font-size: var(--font-size-display);
+		line-height: 1.2;
+		margin: 0;
+
+		.-title & {
+			font-family: var(--font-display-italic);
+		}
+	}
+</style>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,14 +1,9 @@
 <script lang="ts">
 	import Work from '$lib/components/Work.svelte';
+	import Hero from '$lib/components/Hero.svelte';
 
 	import '$lib/styles/index.css';
 </script>
 
-<section>
-	<p>Hello, my name is</p>
-	<h1>Lisa Sundberg</h1>
-	<p>and I'm a</p>
-	<h2>design engineer</h2>
-</section>
-
-<Work />
+<Hero />
+<!-- <Work /> -->
