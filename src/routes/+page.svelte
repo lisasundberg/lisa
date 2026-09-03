a3bd598afb5f711f91aeb3507110f846a36dc04c
Lisa <hello@lisasundberg.com>
2026-09-03 11:13:29 +0200

    MVP new work section

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,9 +1,9 @@
 <script lang="ts">
-	import Work from '$lib/components/Work.svelte';
+	import Work3 from '$lib/components/Work3.svelte';
 	import Hero from '$lib/components/Hero.svelte';
 
 	import '$lib/styles/index.css';
 </script>
 
 <Hero />
-<!-- <Work /> -->
+<Work3 />
